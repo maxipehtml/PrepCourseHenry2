e6a317a8c5ebbebf9e3f2af7a6c2763198be8341
Maxi Pereira <massi.laplata@gmail.com>
2023-08-08 01:52:40 -0300

    Javascript1

---
+++ Js/operadores.js
@@ -0,0 +1,20 @@
+console.log( 3 + 3);
+console.log( 9 % 3);
+
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
